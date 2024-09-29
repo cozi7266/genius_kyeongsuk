65204b548b306fdac0a0ab280c9bf8ea47df4716
KyeungSuk <cozi7266@gmail.com>
2024-09-30 01:08:35 +0900

    240930 Home

---
+++ 240930/BAEKJOON_2293_동전1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BAEKJOON_2293_동전1 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int[] coins = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			coins[i] = sc.nextInt();
+		}
+		
+		int[] dp = new int[k+1];
+		dp[0] = 1;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = coins[i]; j <= k; j++) {
+				dp[j] = dp[j] + dp[j-coins[i]];
+			}
+		}
+		System.out.println(dp[k]);
+	}
+}
