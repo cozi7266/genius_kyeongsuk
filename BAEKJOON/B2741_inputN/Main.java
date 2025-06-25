411ef9ca80d056be5ea008b08fa9bf63b1912912
Kyungsuk Lee <cozi7266@gmail.com>
2025-06-25 23:32:39 +0900

    daily algo

---
+++ BAEKJOON/B2741_inputN/Main.java
@@ -0,0 +1,15 @@
+package B2741_inputN;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			System.out.println(i);
+		}
+	}
+}
