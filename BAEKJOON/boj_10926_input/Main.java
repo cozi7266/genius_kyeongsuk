01ef4c4fb920294a0289772772fb249f0cd0f879
Kyungsuk Lee <cozi7266@gmail.com>
2025-08-08 23:46:09 +0900

    daily algo

---
+++ BAEKJOON/boj_10926_input/Main.java
@@ -0,0 +1,11 @@
+package boj_10926_input;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println(input+"??!");
+	}
+}
